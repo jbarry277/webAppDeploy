48b7d1eca640c213bb6756521410e1a0da31f414
= <jbarrymarron@gmail.com>
2019-08-04 13:46:57 +1200

    api comitted

--- application.py
+++ application.py
@@ -1,6 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+import pyodbc
+import json
 
-@app.route("/")
-def hello():
-    return "Hello Flask, on Azure App Service for Linux"
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import mysql.connector
+
+app = Flask('topAPI')
+api = Api(app)
+
+
+
+
+server = 'topserver1.database.windows.net'
+database = 'topDB'
+username = 'topAdmin'
+password = 'Mitsilancer1'
+driver= '{ODBC Driver 17 for SQL Server}'
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+app.config["DEBUG"] = True
+
+
+
+createStatement = "INSERT INTO movies (id,movieName) VALUES (1,'hhThe_social_network')"
+cursor.execute(createStatement)
+cursor.commit()
+
+
+
+
+class Home(Resource):
+    def get(self):
+        movieData = []
+        result = cursor.execute(selectStatement)
+        for row in result:
+            movieData.append(str(row))
+        return(movieData)
+
+api.add_resource(Home, '/home') # Route_1
+
+#if __name__ == '__main__':
+#     app.run(port='5002')
